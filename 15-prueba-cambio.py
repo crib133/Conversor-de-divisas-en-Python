4c0d8ad2d278cd578399bf7ee72c508e53406ff1
Cristian Ibarra <crib.133@gmail.com>
2026-05-12 02:29:03 -0300

    Update 15-prueba-cambio.py
    Mejoré el conversor de divisas haciendo el código más compacto y automático. Ahora el programa muestra las monedas disponibles usando una lista y un bucle for, recibe el importe y la divisa en una sola línea usando split(), convierte los valores correctamente con float, y utiliza if/elif/else para validar la moneda elegida y mostrar un mensaje de error si la divisa no existe. También agregué round() para mostrar resultados más limpios.

--- 15-prueba-cambio.py
+++ 15-prueba-cambio.py
@@ -1,26 +1,47 @@
-Dinero = int(input("Cantidad a cambiar:$"))
-Monedas = ["euro", "real", "dolar"]
-Dolar = 1399
-Euro = 1642
-Real = 285
-
+Monedas = ["Euros", "Reales", "Dolares"]
 print("Divisas disponibles:")
-
 for i in Monedas:
     print("-", i)
+Dinero, Divisa = input("Ingrese importe en Ars y tipo de cambio: ").split()
+Dolar, Euro, Real = 1399, 1642, 285
 
-Cambio = input("Cambio que desea:")
-if Cambio == "dolar":
-    Resultado = (Dinero / Dolar)
-    print("Su vuelto seria: Usd ", round(Resultado))
+din1 = float(Dinero)
+operador = Divisa
 
-elif Cambio == "euro":
-    Resultado = (Dinero / Euro)
-    print("Su vuelto seria: Eur ", round(Resultado))
-
-elif Cambio == "real":
-    Resultado = (Dinero / Real)
-    print("Su vuelto seria: Brl ", round(Resultado))
+if operador == "Dolares":
+    resultado = (din1 / Dolar)
+    print("Su cambio es: $", round(resultado))
+elif operador == "Euros":
+    resultado = (din1 / Euro)
+    print("Su cambio es: $", round(resultado))
+elif operador == "Reales":
+    resultado = (din1 / Real)
+    print("Su cacambio es:: $", round(resultado))
 
 else:
     print("Divisa no disponible")
+
+# Dinero = int(input("Cantidad a cambiar:$"))
+# Monedas = ["euro", "real", "dolar"]
+# Dolar, Euro, Real = 1399, 1642, 285
+
+# print("Divisas disponibles:")
+
+# for i in Monedas:
+#     print("-", i)
+
+# Cambio = input("Cambio que desea:")
+# if Cambio == "dolar":
+#     Resultado = (Dinero / Dolar)
+#     print("Su vuelto seria: Usd ", round(Resultado))
+
+# elif Cambio == "euro":
+#     Resultado = (Dinero / Euro)
+#     print("Su vuelto seria: Eur ", round(Resultado))
+
+# elif Cambio == "real":
+#     Resultado = (Dinero / Real)
+#     print("Su vuelto seria: Brl ", round(Resultado))
+
+# else:
+#     print("Divisa no disponible")
